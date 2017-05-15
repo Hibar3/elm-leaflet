509c97ca8e09d9438aa947e6879d0455209e84c3
Travis CI <stephane.langlois@scopyleft.fr>
2017-05-15 23:13:16 +0200

    Refactor

--- App.elm
+++ App.elm
@@ -1,39 +1,41 @@
 port module App exposing (..)
-import Html exposing (Html, text, ul, li)
-import Html.App exposing (program)
+
+import Html exposing (Html, text, ul, li, program)
 import Html.Events exposing (onClick)
 import Html.Attributes exposing (id, attribute)
 import Towns
 import Places
 
 
-
 -- Main
 
-main : Program Never
-main = program
-  { init = init
-  , view = view
-  , update = update
-  , subscriptions = subscriptions
-  }
+
+main : Program Never Model Msg
+main =
+    program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = (\_ -> Sub.none)
+        }
 
 
 
 -- Init
 
 
-type alias Model = Towns.Town
+type alias Model =
+    Towns.Town
 
 
-init : (Model, Cmd Msg)
+init : ( Model, Cmd Msg )
 init =
-  ( Towns.defaultTown
-  , Cmd.batch
-    [ portActiveTown Towns.defaultTown
-    , portPlaces Places.places
-    ]
-  )
+    ( Towns.defaultTown
+    , Cmd.batch
+        [ portActiveTown Towns.defaultTown
+        , portPlaces Places.places
+        ]
+    )
 
 
 
@@ -41,46 +43,39 @@ init =
 
 
 port portActiveTown : Towns.Town -> Cmd msg
+
+
 port portPlaces : List Places.Place -> Cmd msg
 
 
 type Msg
-  = SelectTown Towns.Town
+    = SelectTown Towns.Town
 
 
-update : Msg -> Model -> (Model, Cmd Msg)
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  case msg of
-    SelectTown town ->
-       (town, portActiveTown town)
-
-
-
--- SUBSCRIPTIONS
-
-
-subscriptions : Model -> Sub Msg
-subscriptions model =
-  Sub.none
+    case msg of
+        SelectTown town ->
+            ( town, portActiveTown town )
 
 
 
 -- View
 
 
-
 view : Model -> Html Msg
 view model =
-  let
-    townsLi : List Towns.Town -> List (Html Msg)
-    townsLi towns =
-      List.map 
-        (\town ->
-          li 
-            [ attribute "data-selected" (toString (town.name == model.name))
-            , onClick (SelectTown town)
-            ]
-            [text town.name]
-        ) towns
-  in
-    ul [id "towns"] (townsLi Towns.towns)
+    let
+        townsLi : List Towns.Town -> List (Html Msg)
+        townsLi towns =
+            List.map
+                (\town ->
+                    li
+                        [ attribute "data-selected" (toString (town.name == model.name))
+                        , onClick (SelectTown town)
+                        ]
+                        [ text town.name ]
+                )
+                towns
+    in
+        ul [ id "towns" ] (townsLi Towns.towns)
--- Towns.elm
+++ Towns.elm
@@ -1,30 +1,27 @@
 module Towns exposing (..)
 
-
-
 -- Towns
 
 
 type alias Town =
-  { name : String
-  , location : { lat : Float, lng : Float }
-  }
+    { name : String
+    , location : { lat : Float, lng : Float }
+    }
 
 
 defaultTown : Town
 defaultTown =
-  Maybe.withDefault
-    (Town "noWhere" { lat = 47.059407, lng = -0.879787 })
-    (List.head towns)
+    Maybe.withDefault
+        (Town "noWhere" { lat = 47.059407, lng = -0.879787 })
+        (List.head towns)
 
 
 towns : List Town
 towns =
-  [ Town "Montpellier" {lat = 43.610769, lng = 3.876716}
-  , Town "Bordeaux" {lat = 44.837789, lng = -0.579180}
-  , Town "Toulouse" {lat = 43.604652, lng = 1.444209}
-  , Town "Cholet" {lat = 47.059407, lng = -0.879787}
-  , Town "Marseille" {lat = 43.296482, lng = 5.369780}
-  , Town "Lyon" {lat = 45.764043, lng = 4.835659}
-  , Town "Nantes" {lat = 47.215586, lng = -1.541820 }
-  ]
+    [ Town "Montpellier" { lat = 43.610769, lng = 3.876716 }
+    , Town "Bordeaux" { lat = 44.837789, lng = -0.57918 }
+    , Town "Toulouse" { lat = 43.604652, lng = 1.444209 }
+    , Town "Marseille" { lat = 43.296482, lng = 5.36978 }
+    , Town "Lyon" { lat = 45.764043, lng = 4.835659 }
+    , Town "Nantes" { lat = 47.215586, lng = -1.54182 }
+    ]
